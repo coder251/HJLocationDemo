df6391124fd2155a64efc70f1c39dc268e40b475
coder251 <coder251@163.com>
2016-04-09 09:46:42 +0800

    123

---
+++ HJLocationDemo/123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by huangjiong on 16/4/9.
+//  Copyright © 2016年 huangjiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HJLocationDemo/123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  123
+//
+//  Created by huangjiong on 16/4/9.
+//  Copyright © 2016年 huangjiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
